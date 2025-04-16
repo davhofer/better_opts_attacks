4b98105f374777e30fc050caf37d33b9bf487172
nishit <nipandya@ucsd.edu>
2025-04-15 20:38:30 -0700

    created script for runpod

---
+++ README.md
@@ -0,0 +1,4 @@
+# runpod.io instructions - 
+
+1. (Clone Repo) git clone --recursive https://nishitvp:github.com/nishitvp/better_opts_attacks
+2. (Download Models) 
---
+++ initial_setup.py
@@ -0,0 +1,48 @@
+
+import os
+
+# Download data dependencies
+data_urls = [
+    'https://huggingface.co/datasets/hamishivi/alpaca-farm-davinci-003-2048-token/resolve/main/davinci_003_outputs.json',
+    'https://raw.githubusercontent.com/gururise/AlpacaDataCleaned/refs/heads/main/alpaca_data_cleaned.json',
+    'https://raw.githubusercontent.com/tatsu-lab/stanford_alpaca/refs/heads/main/alpaca_data.json',
+    'https://raw.githubusercontent.com/tatsu-lab/alpaca_eval/refs/heads/main/client_configs/openai_configs_example.yaml',
+]
+
+os.makedirs('data', exist_ok=True)
+for data_url in data_urls:
+    data_path = 'data/' + data_url.split('/')[-1]
+    if os.path.exists(data_path): print(data_path, 'already exists.'); continue
+    cmd = 'wget -P data {data_url}'.format(data_url=data_url, data_path=data_path)
+    print(cmd)
+    os.system(cmd)
+
+# Download model dependencies
+model_big_path = "secalign_refactored/secalign_models"
+model_paths = []
+model_paths += [
+    'mistralai/Mistral-7B-Instruct-v0.1_dpo_NaiveCompletion_2024-11-12-17-59-37', # SecAlign Instruct adapters
+    'meta-llama/Meta-Llama-3-8B-Instruct_dpo_NaiveCompletion_2024-11-12-17-59-06',
+]
+model_paths += [
+    'huggyllama/llama-7b_SpclSpclSpcl_None_2024-06-02-00-00-00', # Undefended Alpaca models
+    'mistralai/Mistral-7B-v0.1_SpclSpclSpcl_None_2024-07-20-01-59-11',
+    'meta-llama/Meta-Llama-3-8B_SpclSpclSpcl_None_2024-08-09-17-02-02',
+    'huggyllama/llama-7b_SpclSpclSpcl_NaiveCompletion_2024-02-02-00-00-00', # StruQ Alpaca models
+    'mistralai/Mistral-7B-v0.1_SpclSpclSpcl_NaiveCompletion_2024-07-20-05-46-17',
+    'meta-llama/Meta-Llama-3-8B_SpclSpclSpcl_NaiveCompletion_2024-08-09-12-55-56',
+    'huggyllama/llama-7b_SpclSpclSpcl_None_2024-06-02-00-00-00_dpo_NaiveCompletion_2024-07-06-07-42-23', # SecAlign Alpaca adapters
+    'mistralai/Mistral-7B-v0.1_SpclSpclSpcl_None_2024-07-20-01-59-11_dpo_NaiveCompletion_2024-08-13-17-46-51',
+    'meta-llama/Meta-Llama-3-8B_SpclSpclSpcl_None_2024-08-09-17-02-02_dpo_NaiveCompletion_2024-08-09-21-28-53'
+]
+model_paths = [os.path.join(model_big_path, model_path) for model_path in model_paths]
+
+for model_path in model_paths:
+    if os.path.exists(model_path): print(model_path, 'already exists.'); continue
+    model_dir = model_path.split('/')[0]
+    os.makedirs(model_dir, exist_ok=True)
+    cmd = 'wget -P {model_dir} https://dl.fbaipublicfiles.com/SecAlign/{model_path} && unzip {model_path} -d {model_dir} && rm {model_path}'.format(model_path=model_path + '.zip', model_dir=model_dir)
+    print(cmd)
+    os.system(cmd)
+
+
